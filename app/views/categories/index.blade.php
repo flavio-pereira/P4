b6960789883422f8e9234f44fc4a0bb237742660
Flavio Pereira <flaviopereira@g.harvard.edu>
2014-12-08 20:33:17 -0700

    Fixed css forms and description display

--- app/views/categories/index.blade.php
+++ app/views/categories/index.blade.php
@@ -7,8 +7,10 @@
 		<p class="text-center">You have no books</p>
 		<p class="text-center">Before you type your Recipe, you need to create your Recipe's Book!</p>
 	@else
-			@foreach( $categories as $category )
+			<h4 class="text-center">You have {{$categories->count()}} book</h4>
+			@foreach($categories as $category)
 						<h3><a href="{{ route('categories.show', $category->name) }}">{{ $category->name }}</a>
+						<small>( {{$category->description}} )</small>
 					    {{ Form::open(array('class' => 'form-horizontal', 'method' => 'DELETE', 'route' => array('categories.destroy', $category->name))) }}
 						{{ link_to_route('categories.edit', 'Edit', array($category->name), array('class' => 'btn btn-info btn-xs')) }}
 						{{ Form::submit('Delete', array('class' => 'btn btn-danger btn-xs')) }}
