fb64f5f4fadfcc16808d206860850a28a0aafa7c
Flavio Pereira <flaviopereira@g.harvard.edu>
2014-12-12 15:35:46 -0700

    Fixed spell

--- app/views/layouts/home.blade.php
+++ app/views/layouts/home.blade.php
@@ -13,7 +13,7 @@
             <div class="col-md-4">
                 <p> Welcome to your Virtual Recipe Books! Here you can register all your recipes and easily access them when you need.  
                 You can divide your recipes in books and then put create all your recipes under your favorite book.  
-                Go ahead and register your account <a href='/signup'>here</a> and then starting creating your recipes!</p>
+                Go ahead and register your account <a href='/signup'>here</a> and start creating your recipes!</p>
             </div>
             <!-- /.col-md-4 -->
         </div>
