1cbcc2377c49ca0b2617b63f7f8c5183fd735114
Flavio Pereira <flaviopereira@g.harvard.edu>
2014-12-16 11:08:20 -0700

    Fixed the project name

--- app/views/layouts/home.blade.php
+++ app/views/layouts/home.blade.php
@@ -3,7 +3,7 @@
 @section('body')
     
   <div class="container">
-    <h1>Welcome to Recipe Book</h1>
+    <h1>Welcome to My Recipe Book</h1>
 
     <div class="row">
             <div class="col-md-8">
@@ -11,7 +11,7 @@
             </div>
             <!-- /.col-md-8 -->
             <div class="col-md-4">
-                <p> Welcome to your Virtual Recipe Books! Here you can register all your recipes and easily access them when you need.  
+                <p> Welcome to My recipe book! Here you can register all your recipes and easily access them when you need.  
                 You can divide your recipes in books and then put create all your recipes under your favorite book.  
                 Go ahead and register your account <a href='/signup'>here</a> and start creating your recipes!</p>
             </div>
