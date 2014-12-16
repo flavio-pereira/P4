faac989e0154b8dcd5bcfd6a5b005a290ab18ba6
Flavio Pereira <flaviopereira@g.harvard.edu>
2014-12-16 16:19:34 -0700

    Fixed typos

--- app/views/layouts/home.blade.php
+++ app/views/layouts/home.blade.php
@@ -12,7 +12,7 @@
             <!-- /.col-md-8 -->
             <div class="col-md-4">
                 <p> Welcome to My recipe book! Here you can register all your recipes and easily access them when you need.  
-                You can divide your recipes in books and then put create all your recipes under your favorite book.  
+                You can divide your recipes in books and then create all your recipes under your favorite book.  
                 Go ahead and register your account <a href='/signup'>here</a> and start creating your recipes!</p>
             </div>
             <!-- /.col-md-4 -->
